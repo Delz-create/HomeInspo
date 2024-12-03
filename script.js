90d37b593b60570d2379b1bffac1947e8fdfc29c
Delz <115114479+Delz-create@users.noreply.github.com>
2024-12-03 13:01:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("navbar").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("navbar").style.width = "0";    
+  }
+
+  function clicked(){
+    document.getElementById("navbar").style.width = "0";
+  }
